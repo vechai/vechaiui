a2acd7210e539eed6faa0764f562638115fc52e5
pepelele <getvechai@gmail.com>
2021-06-16 20:36:02 +0700

    feat: add message components

---
+++ packages/core/src/components/message.js
@@ -0,0 +1,29 @@
+module.exports = Message = () => ({
+  ".message": {
+    "@apply flex items-center px-3 py-1.5 m-1 text-sm rounded-md shadow-sm w-max break-words": {},
+    "@apply text-neutral-900 bg-white border border-neutral-200": {},
+    // dark
+    "@apply dark:text-neutral-100 dark:bg-neutral-700 dark:border-neutral-600": {},
+  },
+
+  ".message-icon": {
+    "@apply mr-2": {},
+
+    "&-info": {
+      "--tw-text-opacity": "1",
+      color: "rgba(59, 130, 246, var(--tw-text-opacity))",
+    },
+    "&-success": {
+      "--tw-text-opacity": "1",
+      color: "rgba(34, 197, 94, var(--tw-text-opacity))",
+    },
+    "&-error": {
+      "--tw-text-opacity": "1",
+      color: "rgba(239, 68, 68, var(--tw-text-opacity))",
+    },
+    "&-warning": {
+      "--tw-text-opacity": "1",
+      color: "rgba(234, 179, 8, var(--tw-text-opacity))",
+    },
+  },
+});
