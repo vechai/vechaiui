a7fa04bc55c177a88687f1b9e78f1f30876945d2
pepelele <getvechai@gmail.com>
2021-07-02 20:04:45 +0700

    ref: rewrite spinner

--- packages/spinner/src/spinner.tsx
+++ packages/spinner/src/spinner.tsx
@@ -5,8 +5,6 @@ import * as React from "react";
 import useSpinClass from "./styles";
 
 export interface ISpinProps extends DefaultProps {
-  /* Set the spinner color */
-  color?: string;
   /* Size of the spinner */
   size?: "xs" | "sm" | "md" | "lg" | "xl";
 }
@@ -15,36 +13,47 @@ export interface SpinProps
   extends React.HTMLAttributes<SVGSVGElement>,
     ISpinProps {}
 
-export const Spinner = React.forwardRef<SVGSVGElement, SpinProps>((props, ref) => {
-  const { color = "currentColor", size = "sm", className, ...rest } = props;
-  const classes = useSpinClass({ size });
+export const Spinner = React.forwardRef<SVGSVGElement, SpinProps>(
+  (props, ref) => {
+    const { size = "sm", className, ...rest } = props;
+    const classes = useSpinClass({ size });
 
-  return (
-    <svg
-      ref={ref}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      className={cx(classes, className)}
-      {...rest}
-    >
-      <circle
-        className="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
+    return (
+      <svg
+        ref={ref}
+        xmlns="http://www.w3.org/2000/svg"
         stroke="currentColor"
-        strokeWidth="4"
-      ></circle>
-      <path
-        className="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
-    </svg>
-  );
-});
+        className={cx(classes, className)}
+        fill="none"
+        viewBox="0 0 66 66"
+        {...rest}
+      >
+        <circle
+          cx="33"
+          cy="33"
+          fill="none"
+          r="28"
+          stroke="currentColor"
+          strokeWidth="10"
+          className="opacity-30"
+        />
+        <circle
+          cx="33"
+          cy="33"
+          fill="none"
+          r="28"
+          stroke="currentColor"
+          strokeDasharray="40, 134"
+          strokeDashoffset="325"
+          strokeLinecap="round"
+          strokeWidth="10"
+          className="opacity-70"
+        />
+      </svg>
+    );
+  }
+);
 
 if (__DEV__) {
-  Spinner.displayName = 'Spinner';
+  Spinner.displayName = "Spinner";
 }
