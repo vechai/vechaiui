fd20bd9bfd34627bc8281bb13d8c39e239c9cb59
pepelele <getvechai@gmail.com>
2021-07-23 19:32:58 +0700

    fix(kbd): background color

--- packages/core/src/components/kbd.js
+++ packages/core/src/components/kbd.js
@@ -2,8 +2,8 @@ module.exports = Kbd = () => ({
   ".kbd": {
     "@apply inline-flex justify-center items-center capitalize text-xs p-1 rounded h-5 whitespace-nowrap font-sans": {},
     "minWidth": "1.25rem",
-    "@apply bg-neutral-100 text-neutral-900": {},
+    "@apply bg-blackAlpha-100 text-neutral-900": {},
     // dark
-    "@apply dark:bg-neutral-700 dark:text-neutral-300": {},
+    "@apply dark:bg-whiteAlpha-300 dark:text-neutral-300": {},
   },
 });
