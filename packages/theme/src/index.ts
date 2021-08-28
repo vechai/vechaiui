5d5a29e1458575e8b7dd3fef57c710b0a9d2ef7a
pepelele <getvechai@gmail.com>
2021-08-28 20:12:57 +0700

    chore: export create theme variable

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./vechai-provider";
 export * from "./create-memo-class";
+export * from "./create-theme-vars";
 export * from "./default-theme";
 export * from "./colors";
 export * from "./types";
